a6708bfae48449815d004c2ec887d5810eabe3c3
Egorov Roman <r.egorov@vbankcenter.ru>
2020-09-15 19:07:52 +0300

    Add main routes

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,27 @@
+import { PostPageComponent } from './post-page/post-page.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { MainLayoutComponent } from './shared/components/main-layout/main-layout.component';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
+
+
+const routes: Routes = [
+  {
+    path: '', component: MainLayoutComponent, children: [
+      {path: '', redirectTo: '/', pathMatch: 'full'},
+      {path: '', component: HomePageComponent},
+      {path: 'post/:id', component: PostPageComponent}
+    ]
+  },
+  {
+    path: 'admin', loadChildren: './admin/admin.module#AdminModule'
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes, {
+    preloadingStrategy: PreloadAllModules
+  })],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
